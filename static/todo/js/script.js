60ef6414a8b368e7c9c8203ce80df6541cb6fdfe
tellefk <tellef94@hotmail.com>
2023-06-05 08:38:33 +0200

    fixes

--- static/todo/js/script.js
+++ static/todo/js/script.js
@@ -68,28 +68,25 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 
-
 function openCity(evt, cityName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Hide all tab contents
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
+  // Remove the "active" class from all tab links
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
+  // Show the selected tab content and add "active" class to the selected tab link
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-
 
 
 ///////////////////////////////////////
